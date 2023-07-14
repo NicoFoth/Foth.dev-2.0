e103b543e6e0dd9516d7194f165913e30b24c5eb
Nico Foth <nico@foth.dev>
2023-07-14 16:53:37 +0200

    Completely redesigned the csgo datamodels

--- csgo/admin.py
+++ csgo/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import CsgoPlayer
+from .models import *
 
 
-admin.site.register(CsgoPlayer)
+admin.site.register(CSSeason)
+admin.site.register(CSMatch)
+admin.site.register(CSPlayer)
+admin.site.register(CSPlayerMatch)
+admin.site.register(CSMap)
+admin.site.register(CSMode)
+admin.site.register(CSRank)
