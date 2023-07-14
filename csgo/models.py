fafa6fa4f5ced5dcb0b7e28a543d2f3c9d2a752e
Nico Foth <nico@foth.dev>
2023-07-15 01:23:29 +0200

    Improvements to the cs data model

--- csgo/models.py
+++ csgo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CSRank(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
     imageLink = models.CharField(max_length=128)
     minElo = models.IntegerField(default=0)
     maxElo = models.IntegerField(default=0)
@@ -11,21 +11,21 @@ class CSRank(models.Model):
         return f"{self.name}"
     
 class CSMap(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
     
     def __str__(self):
         return f"{self.name}"
     
 
 class CSMode(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
     
     def __str__(self):
         return f"{self.name}"
     
 
 class CSPlayer(models.Model):
-    steam_id = models.CharField(max_length=18)
+    steam_id = models.CharField(max_length=18, unique=True)
     name = models.CharField(max_length=32)
 
     def __str__(self):
@@ -66,6 +66,9 @@ class CSPlayerMatch(models.Model):
     enemiesFlashed = models.IntegerField(default=0)
     utilityDamage = models.IntegerField(default=0)
 
+    class Meta:
+        unique_together = ('player', 'match')
+
     def __str__(self):
         return f"{self.player} - {self.match}"
     
@@ -74,6 +77,9 @@ class CSPlayerSeasonElo(models.Model):
     player = models.ForeignKey(CSPlayer, on_delete=models.CASCADE)
     season = models.ForeignKey(CSSeason, on_delete=models.CASCADE)
     elo = models.IntegerField(default=1500)
+
+    class Meta:
+        unique_together = ('player', 'season')
 
     def __str__(self):
         return f"{self.player} - {self.elo}"
