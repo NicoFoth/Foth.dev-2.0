e05fc59cf6e23893258a8636b07ff7c12b11303e
Nico Foth <nico@foth.dev>
2023-07-15 02:07:18 +0200

    Added the first version of the season overview

--- csgo/urls.py
+++ csgo/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.show_csgo_stats, name="show_csgo_stats"),
+    path('', views.cs_overview, name="cs_overview"),
     path('select-players', views.select_players, name="select_players"),
-    path('<str:steam_id>', views.player_profile, name="player_profile")
+    path('seasons/<str:season_id>', views.season_overview, name="season_overview"),
 ]
